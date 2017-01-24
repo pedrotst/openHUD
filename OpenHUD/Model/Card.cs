21c168cb69b1c51f097deddc58ad040cabe71d4b
pedrotst <abreu223@hotmail.com>
2017-01-24 17:49:42 -0200

    Structure MVC+Persistence

---
+++ OpenHUD/Model/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenHud
+{
+    class Card
+    {
+        public enum Suits { Hearts = 'h', Spades = 's', Diamonds = 'd', Clubs = 'c'};
+
+        public enum Ranks {
+            Deuce = '2', Three, Four, Five, Six, Seven, Eight, Nine,
+            Ten = 'T', Jack = 'J', Queen = 'Q', King = 'K', Ace = 'A'
+        }
+
+        public Tuple<char, char> card;
+
+        public Card(string card)
+        {
+            var cardArray = card.ToCharArray();
+            var cardCh = cardArray[0];
+            var cardSuit = cardArray[1];
+            this.card = new Tuple<char, char>(cardCh, cardSuit);
+        }
+
+    }
+}
---
+++ OpenHUD/Model/Hand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenHud
+{
+    class Hand
+    {
+        public double handNumber;
+        public string pokerType;
+        public double smallBlind;
+        public double bigBlind;
+        public string currency;
+        public string date;
+        public string tableName;
+        public string maxSeat;
+        public string buttonSeat;
+        public List<Player> players;
+
+        public Hand(string handNumber, string pokerType, string smallBlind, string bigBlind, string currency, string date, string tableName, string maxSeat, string buttonSeat, List<Player> players,
+            string cardsOwner, string cards)
+        {
+            this.handNumber = double.Parse(handNumber);
+            this.pokerType = pokerType;
+            this.smallBlind = double.Parse(smallBlind, CultureInfo.InvariantCulture);
+            this.bigBlind = double.Parse(bigBlind, CultureInfo.InvariantCulture);
+            this.currency = currency;
+            this.date = date;
+            this.tableName = tableName;
+            this.maxSeat = maxSeat;
+            this.buttonSeat = buttonSeat;
+            var card1 = new Card(cards.Substring(0,2));
+            var card2 = new Card(cards.Substring(3));
+            var cardPlayer = players.Find(p => p.name == cardsOwner);
+            cardPlayer.cards = new Card[] { card1, card2 };
+            this.players = players;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("----------------------- Hand #" + handNumber + " ----------------------- ");
+            Console.WriteLine(pokerType);
+            Console.WriteLine("Blinds: ${0}/${1}", smallBlind, bigBlind);
+            Console.WriteLine(date);
+            Console.WriteLine(tableName);
+            Console.WriteLine(maxSeat + " Max");
+            Console.WriteLine("Button: " + buttonSeat);
+            players.ForEach(p => p.Print());
+            Console.WriteLine();
+        }
+
+    }
+}
